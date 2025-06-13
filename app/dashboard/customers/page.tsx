f1b2d8460d02d961fa845f5b1ed269f2890f7772
Feisal84 <e02@deenly.io>
2025-06-13 21:03:12 +0200

    initial commit

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,10 @@
+
+
+export default function page() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-4xl font-bold">Customers</h1>
+      <p className="mt-4 text-lg">Manage your customers here.</p>
+    </div>
+  );
+}
